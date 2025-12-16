cd74e4bbad36974d1efe6e5c86dc112f5b0ee6bc
صالح خليل <facebookg2065@gmail.com>
2025-12-16 17:46:47 +0000

    ارجوا منك مزامنه المشروع تلقائيًا

--- next.config.ts
+++ next.config.ts
@@ -17,8 +17,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-        ,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
